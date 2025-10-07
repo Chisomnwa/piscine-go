f0b695b25323c230ca8df8b3cd07dfaa23ea679b
cnnamani <nnamanichisomnc@gmail.com>
2025-10-07 12:50:44 +0100

    Added the myfamily.sh file

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://acad.learn2earn.ng/assets/superhero/all.json | jq --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .connections.relatives' | tr -d '""'
+curl -s https://acad.learn2earn.ng/assets/superhero/all.json | jq ".[] | select(.id==$HERO_ID) | .connections.relatives" | tr -d '""'
