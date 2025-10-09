6b5b2bac3051c45b490b2c702f59e1eb0308211c
cnnamani <nnamanichisomnc@gmail.com>
2025-10-09 08:55:39 +0100

    Edited the main.go file

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for c := 'z'; c <= 'a'; c-- {
-		z01.PrintRune(c)
+	for r := 'z'; r >= 'a'; r-- {
+		z01.PrintRune(r)
 	}
 	z01.PrintRune('\n')
 }
