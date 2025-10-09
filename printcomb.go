d813a1db58fab6c1f553c1c91a8e6327257410c8
cnnamani <nnamanichisomnc@gmail.com>
2025-10-09 12:29:33 +0100

    My second go function

--- printcomb.go
+++ printcomb.go
@@ -3,12 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for x := '0'; x <= '9'; x++ {
-		for y := '1'; y < x; y++ {
-			for z := '2'; z < y; z++ {
-				z01.PrintRune(z)
-				z01.PrintRune(y)
+	for x := '0'; x <= '7'; x++ {
+		for y := x + 1; y <= '8'; y++ {
+			for z := y + 1; z <= '9'; z++ {
 				z01.PrintRune(x)
+				z01.PrintRune(y)
+				z01.PrintRune(z)
 				if !(z == '7' && y == '8' && x == '9') {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
