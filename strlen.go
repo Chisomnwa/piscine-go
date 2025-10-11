e52f815d652eb75fc14215198da2cfdbc3184460
cnnamani <nnamanichisomnc@gmail.com>
2025-10-11 10:02:27 +0100

    Created the strlen function

---
+++ strlen.go
@@ -0,0 +1,13 @@
+/*
+Write a function that counts the runes of a string and that returns that count.
+*/
+
+package piscine
+
+func StrLen(str string) int {
+	count := 0
+	for range str {
+		count++
+	}
+	return count
+}
