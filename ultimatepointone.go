277b8a9b0c67b32de3c0876e51f887557cc7ec38
cnnamani <nnamanichisomnc@gmail.com>
2025-10-11 09:45:47 +0100

    Created the ultimatepointone function

---
+++ ultimatepointone.go
@@ -0,0 +1,10 @@
+/*
+Write a function that takes a pointer to a pointer to a pointer to an
+ int as argument and gives to this int the value of 1
+*/
+
+package piscine
+
+func UltimatePointOne(n ***int) {
+	***n = 1
+}
