87a31be040850f952944008dd067745a14c844ce
cnnamani <nnamanichisomnc@gmail.com>
2025-10-11 09:52:31 +0100

    Created the ultimatedivmod function

---
+++ ultimatedivmod.go
@@ -0,0 +1,20 @@
+/*
+Create the following function
+
+func UltimateDivMod(a *int, b *int) {
+
+}
+
+UltimateDivmod should divide the deferenced value of a by the deferenced value of b.
+- store the result of the division in the fmt which a points to
+- Store the remainder of the division in the int which b points to
+*/
+
+package piscine
+
+func UltimateDivMod(a *int, b *int) {
+	div := *a / *b
+	mod := *a % *b
+	*a = div
+	*b = mod
+}
