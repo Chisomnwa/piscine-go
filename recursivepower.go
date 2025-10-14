cb43a01afee08712953b3059e0b9c2edbd825b52
cnnamani <nnamanichisomnc@gmail.com>
2025-10-14 11:22:44 +0100

    Added the recursive power function

---
+++ recursivepower.go
@@ -0,0 +1,17 @@
+package piscine
+
+func RecursivePower(nb int, power int) int {
+	// iIF power is less than zero
+	if power < 0 {
+		return 0
+	}
+
+	// When power equals zero because anything raised 
+	// to power zero is 1
+	if power == 0 {
+		return 1
+	}
+
+	// Calling the recursive
+	return nb * RecursivePower(nb, power - 1)
+}
