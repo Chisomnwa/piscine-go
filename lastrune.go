01ee146963cdc238051eb4439dd0511f9c08a270
cnnamani <nnamanichisomnc@gmail.com>
2025-10-14 16:26:07 +0100

    Created a function that prints the last rune of a string

---
+++ lastrune.go
@@ -0,0 +1,6 @@
+package piscine
+
+func LastRune(s string) rune {
+	runes := []rune(s) // convert   strings to slice of bytes to habdle correctlly
+	return runes[len(runes)-1] // Return the last rune
+	}
