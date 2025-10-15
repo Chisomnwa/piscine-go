4ffefd27fc0df9b99f2261e5bd6c15bd9254657f
cnnamani <nnamanichisomnc@gmail.com>
2025-10-15 09:26:30 +0100

    Added the Compare function

---
+++ compare.go
@@ -0,0 +1,20 @@
+package piscine
+
+// Write a function that behaves like the compare function
+func Compare(a, b string) int {
+	for i := 0; i < len(a) && i < len(b); i++ {
+		if a[i] != b[i] {
+			if a[i] < b[i] {
+				return -1
+			}
+			return 1
+		}
+	}
+
+	if len(a) == len(b) {
+		return 0
+	} else if len(a) < len(b) {
+		return -1
+	}
+	return 1
+}
