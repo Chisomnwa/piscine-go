4ba7338381cd1aa023b8ef73081673e5fc5c616b
cnnamani <nnamanichisomnc@gmail.com>
2025-10-15 09:21:38 +0100

    Added the lastrune function

--- lastrune.go
+++ lastrune.go
@@ -1,6 +1,11 @@
 package piscine
 
+// LastRune returns the last rune (Unicode code point) of a string.
+// if the string is empty, it returns rune(0)
 func LastRune(s string) rune {
-	runes := []rune(s)         // convert   strings to slice of bytes to habdle correctlly
-	return runes[len(runes)-1] // Return the last rune
+	var last rune
+	for _, letter := range s {
+		last = letter
+	}
+	return last
 }
