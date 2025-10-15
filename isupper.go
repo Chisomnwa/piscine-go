12031b1b996c9642be1e4aa4cd66776177d5b640
cnnamani <nnamanichisomnc@gmail.com>
2025-10-15 09:44:57 +0100

    Added the isupper function

--- isupper.go
+++ isupper.go
@@ -2,10 +2,10 @@
 package piscine
 
 func IsUpper(s string) bool {
-	for _, letter := range s { // loop through each rune (character)
-		if letter < 'A' || letter > 'Z' { // if any character is not uppercase
+	for _, letter := range s {              // loop through each rune (character)
+		if letter < 'A' || letter > 'Z' { 	// if any character is not uppercase
 			return false
 		}
 	}
-	return true // if all characters were uppercase
+	return true 							// if all characters were uppercase
 }
