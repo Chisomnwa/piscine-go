0ee503ff61754eaea232c4e3c3ef4da0079aec0f
cnnamani <nnamanichisomnc@gmail.com>
2025-10-16 10:53:43 +0100

    Formmated the function

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -16,11 +16,14 @@ func main() {
 		args = args[1:] // Remove the flag from arguments
 	}
 
+	printed := false // Tracks if we printed anything at all
+
 	for _, arg := range args {
 		num, ok := toInt(arg) // convert string to integer
 		if !ok || num < 1 || num > 26 {
 			// Invalid argument - prints space
 			z01.PrintRune(' ')
+			printed = true
 			continue
 		}
 
@@ -36,18 +39,23 @@ func main() {
 		printed = true
 	}
 
+	// Only print newline if something was printed
 	if printed {
 		z01.PrintRune('\n')
 	}
 }
 
+// Converts a numeric string to an integer manually (no strconv)
 func toInt(s string) (int, bool) {
 	num := 0
+	if s == "" { // empty string is invalid
+		return 0, false
+	}
 	for _, r := range s {
-		if r < '0' || r > '9' {
+		if r < '0' || r > '9' { // only digits allowed
 			return 0, false
 		}
-		num = num*10 + int(r-'0')
+		num = num*10 + int(r-'0') // build integer manually
 	}
 	return num, true
 }
