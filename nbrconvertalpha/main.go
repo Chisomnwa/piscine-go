aa88f2f47475454872128f5165f6f6a2c8ce5cce
cnnamani <nnamanichisomnc@gmail.com>
2025-10-16 09:50:10 +0100

    CReated a function that converted a n position to an alphabet

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+
+	"strconv"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	upper := false
+
+	// Check for the upper flag when passed as one of the command line args
+	if len(args) > 0 && args[0] == "--upper" {
+		upper = true
+		args = args[1:] // Remove the flag from arguments
+	}
+
+	for _, arg := range args {
+		num, err := strconv.Atoi(arg) // convert string to integer
+		if err != nil || num < 1 || num > 20 {
+			// Invalid argument - prints space
+			z01.PrintRune(' ')
+			continue
+		}
+
+		// Convert number to corresponding letter
+		var letter rune
+		if upper {
+			letter = rune('A' + num -1) // uppercase letters
+		} else {
+			letter = rune('a' + num -1) // lowercase letters
+		}
+		
+		z01.PrintRune(letter)
+	}
+	z01.PrintRune('\n')
+}
