c95ea8906b2407f85378f17cf9c266990169117c
cnnamani <nnamanichisomnc@gmail.com>
2025-10-17 17:10:10 +0100

    Created a function that prints each element of the slice in a seperate line

--- printwordstables.go
+++ printwordstables.go
@@ -1,1 +1,12 @@
 package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintWordsTables(a []string) {
+	for _, word := range a { // loop through each string in the slice
+		for _, char := range word { // loop through each character (rune)
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+	}
+}
