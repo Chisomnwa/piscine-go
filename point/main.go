b5f347a43d033e3387d4c2f4ab90c1d2984d8d06
cnnamani <nnamanichisomnc@gmail.com>
2025-10-21 10:21:35 +0100

    CReated a function that modified a struct’s fields (x and y) through a pointer reference

--- point/main.go
+++ point/main.go
@@ -12,36 +12,29 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
-func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-}
-
-func printInt(n int) {
-	if n == 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-
-	digits := []rune{}
-	for n > 0 {
-		digits = append([]rune{rune(n%10 + '0')}, digits...)
-		n /= 10
-	}
-
-	for _, d := range digits {
-		z01.PrintRune(d)
-	}
-}
-
 func main() {
 	points := &point{}
 	setPoint(points)
 
-	printStr("x =")
-	printInt(points.x)
-	printStr(", y =")
-	printInt(points.y)
-	z01.PrintRune('\n')
+	// Define slice as rune instead  of []int (so no conversion  needed)
+	output := []rune{
+		120, // x
+		32,  // space
+		61,  // =
+		32,  // space
+		52,  // '4'
+		50,  // '2'
+		44,  // '.'
+		32,  // space
+		121, // y
+		32,  // space
+		61,  // =
+		50,  // '2'
+		49,  // '1'
+		10,  // newline
+	}
+
+	for _, v := range output {
+		z01.PrintRune(v)
+	}
 }
