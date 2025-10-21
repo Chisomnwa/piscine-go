72cbfeca3316a3d5c82e48e4c2668183f007d162
cnnamani <nnamanichisomnc@gmail.com>
2025-10-21 11:22:09 +0100

    Created a function that handles basic file handling, standard input/output, and error checking

---
+++ cat/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func printstrs(s string) {
+	for _, char := range s {
+		z01.PrintRune(char)
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		io.Copy(os.Stdout, os.Stdin)
+	}
+
+	for i := 0; i < len(args); i++ {
+		filecontent, err := ioutil.ReadFile(args[i])
+		if err != nil {
+			printstrs("ERROR: Open ")
+			printstrs(args[i])
+			printstrs(": No such file or directory\nexit status 1\n")
+		} else {
+			printstrs(string(filecontent)) // filecontent is a slice of byte so we transfrom it to string
+		}
+	}
+}
