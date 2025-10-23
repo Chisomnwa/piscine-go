345bd8914f74ca72af9889a21afdbea490fa2a17
cnnamani <nnamanichisomnc@gmail.com>
2025-10-23 12:30:57 +0100

    Add files to Github

--- countif.go
+++ countif.go
@@ -1,12 +1,12 @@
 package piscine
 
 func CountIf(f func(string) bool, tab []string) int {
-	count := 0
-	for _, str := range tab {
-		if f(str) {
-			count++
+	var counter int = 0
+
+	for _, val := range tab {
+		if f(val) {
+			counter++
 		}
 	}
-	return count
+	return counter
 }
-
