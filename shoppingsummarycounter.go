80f1939db38c6ce0a4921415d5d30470899e10b7
cnnamani <nnamanichisomnc@gmail.com>
2025-10-24 09:07:03 +0100

    IMplemented a fuunction that returns the summary of bought items

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,21 +1,24 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	result := make(map[string]int) // We create an empty string
-
+	result := make(map[string]int)
 	word := ""
+
 	for i := 0; i < len(str); i++ {
 		char := str[i]
-		if char != ' ' { // Let's build each word character by character
+
+		if char != ' ' {
 			word += string(char)
 		} else {
-			if word != "" { // if word is completed
-				result[word]++ // increment its count
+			// Only count the word if it's not empty
+			if word != "" {
+				result[word]++
 				word = ""
 			}
 		}
 	}
-	// Handle the last word (since it may not end with a space)
+
+	// Handle the last word (if there’s no trailing space)
 	if word != "" {
 		result[word]++
 	}
