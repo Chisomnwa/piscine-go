188f63d3f86d7b174fbe72c13298b3bd84d5914e
cnnamani <nnamanichisomnc@gmail.com>
2025-10-27 12:43:09 +0100

    Formatted the function

---
+++ dealapackofcards.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "fmt"
+
+func DealAPackofCards(deck []int) {
+	cardsPerPlayer := len(deck) / 4 // 12 / 4 = 3
+
+	for player := 0; player < 4; player++ {
+		fmt.Printf("Player %d: ", player+1)
+
+		// Print each player's 3 cards
+		for i := 0; i < cardsPerPlayer; i++ {
+			cardIndex := player*cardsPerPlayer + i
+			fmt.Printf("%d", deck[cardIndex])
+
+			// Add comma and space after each card except the last
+			if i < cardsPerPlayer-1 {
+				fmt.Print(", ")
+			}
+		}
+		fmt.Printf("/n")
+	}
+
+}
