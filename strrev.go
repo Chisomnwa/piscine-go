dea0cc9042c66a4784c3ba9ae754ed4b13f874f1
cnnamani <nnamanichisomnc@gmail.com>
2025-10-27 13:47:52 +0100

    Added a new line at the end of the file

--- strrev.go
+++ strrev.go
@@ -1,9 +1,18 @@
 package piscine
 
+// func StrRev(s string) string {
+// 	runes := []rune(s) // convert string to rune slice to handle Unicode correctly
+// 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+// 		runes[i], runes[j] = runes[j], runes[i] // swap runes
+// 	}
+// 	return string(runes) // convert back to string
+// }
+
 func StrRev(s string) string {
-	runes := []rune(s) // convert string to rune slice to handle Unicode correctly
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i] // swap runes
+	runes := []rune(s)
+	result := ""
+	for i := len(runes) - 1; i >= 0; i-- { // Startsiterating from the last index
+		result += string(s[i])
 	}
-	return string(runes) // convert back to string
+	return result
 }
