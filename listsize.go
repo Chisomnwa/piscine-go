299f4cb2a0da2b990ae4580c39e42cd4c75ceca7
cnnamani <nnamanichisomnc@gmail.com>
2025-10-28 09:24:17 +0100

    Implemented a function that returns the number of elements in a linked list l

--- listsize.go
+++ listsize.go
@@ -1,21 +1,12 @@
 package piscine
 
-type NOdeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type LIst struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListSize(l *List) int {
-	count := 0      // start a counter
-	it := l.Head    // start from the first node
-	for it != nil { // loop until we reach the end (nil)
-		count++      // increase count for each node
-		it = it.Next // move to the next node
+	count := 0
+	current := l.Head
+
+	for current != nil {
+		count++
+		current = current.Next
 	}
-	return count // return the total number of nodes
+	return count
 }
